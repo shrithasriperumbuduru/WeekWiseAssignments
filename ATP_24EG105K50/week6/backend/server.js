6910dd0a1ddd96d3ba6907ce0e8c8d2200d6f257
Shritha <Shrithasriperumbuduru@gmail.com>
2026-05-20 13:41:08 +0530

    Fixed CORS deployment issue

--- ATP_24EG105K50/week6/backend/server.js
+++ ATP_24EG105K50/week6/backend/server.js
@@ -4,6 +4,7 @@ import { empRoute } from "./API/empApp.js";
 import cors from "cors";
 
 const app = exp();
+
 app.use(
   cors({
     origin: [
@@ -11,10 +12,11 @@ app.use(
       "http://localhost:5174",
       "https://mern-app-emps.vercel.app",
       "https://mern-app-emps-k1ideeqbc-madhurimaam12s-projects.vercel.app",
-      "https://mern-app-emps-qq2v17jea-madhurimaam12s-projects.vercel.app"
+      "https://mern-app-emps-qq2v17jea-madhurimaam12s-projects.vercel.app",
+      "https://blogapp-ten-sooty.vercel.app",
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -23,12 +25,19 @@ app.use("/emp-api", empRoute);
 
 const connectDB = async () => {
   try {
-    const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://madhurima:madhurima@clusterO.farh4i4.mongodb.net/?appName=ClusterO";
+    const MONGODB_URI =
+      process.env.MONGODB_URI ||
+      "mongodb+srv://madhurima:madhurima@clusterO.farh4i4.mongodb.net/?appName=ClusterO";
+
     await connect(MONGODB_URI);
+
     console.log("DB connected");
-    
+
     const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => console.log(`server listening on port ${PORT}..`));
+
+    app.listen(PORT, () =>
+      console.log(`server listening on port ${PORT}..`)
+    );
   } catch (err) {
     console.log("err in DB connection", err.message);
   }
@@ -38,6 +47,7 @@ connectDB();
 
 app.use((err, req, res, next) => {
   console.log("err in middleware:", err.message);
+
   res.status(err.status || 500).json({
     message: "error",
     reason: err.message,
